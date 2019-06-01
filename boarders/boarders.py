4bc0e485a1eb7d293af741cd7910abd2d13c7c1a
Amit Nizri <amitnzri@gmail.com>
2019-06-01 18:14:58 +0300

    Updates

--- boarders/boarders.py
+++ boarders/boarders.py
@@ -16,7 +16,8 @@ Easy and simple module for creating message boxes for CLI.
 #///////////////TODO:///////////////////
 #1.Add special boxes types.
 #2.Add an option for box in a box mode.
-
+#3.fix the bug when the right corner jumps forward.
+#4.rebuild update dimensions function.
 class createBox:
     
     def __init__(self,width=1,height=1):
@@ -34,8 +35,8 @@ class createBox:
         self.__width = width
         self.__height = height
         self.__content = []
-        self.__horizontal_symbol = '*'
-        self.__vertical_symbol = '*'
+        self.__hSymbol = '*'
+        self.__vSymbol = '*'
         self.__textAllign = '<' 
         self.__margin = 1 
         self.__hPadding = 1
@@ -61,14 +62,17 @@ class createBox:
         elif side is None:
             return None
         else:
-            print('Wrong Allignment! Enter: left,right or center..')
+            raise  ValueError('Wrong Allignment! Enter: left,right or center..')
             return None 
 
     def __updateDimensions(self): 
-        line_width = self.__getMaxLineLen() + self.__margin*2 + len(self.__vertical_symbol*2)
+        line_width = self.__getMaxLineLen() + self.__margin*2 + len(self.__vSymbol*2) 
         num_of_lines = len(self.__content)
         self.__height = self.__height if num_of_lines < self.__height else num_of_lines
-        self.__width = self.__width if line_width < self.__width else line_width 
+        self.__width = self.__width if line_width < self.__width else line_width
+        
+        #Deals with hSymbols lengths that doesn't fit that width.
+        self.__width +=(len(self.__hSymbol)-(self.__width%len(self.__hSymbol)))/len(self.__hSymbol) 
                       
 
     def style(self,width=None ,height= None,allign = None ,
@@ -89,20 +93,19 @@ class createBox:
         self.__height = abs(int(height or self.__height))
         self.__width = abs(int(width or self.__width))
         self.__textAllign = self.__setAllignment(allign) or self.__textAllign
-        self.__horizontal_symbol = hSymbol or self.__horizontal_symbol
-        self.__vertical_symbol = vSymbol or self.__vertical_symbol
+        self.__hSymbol = str(hSymbol or self.__hSymbol)
+        self.__vSymbol = str(vSymbol or self.__vSymbol)
         self.__margin = int(margin or self.__margin)
         self.__hPadding = int(hPadding or self.__hPadding)
 
-    def append(self,string):
-        '''Adds a new line to the box.'''
-        self.__content.append(str(string))
+    def append(self,msg):
+        '''Adds new lines to the box.'''
+        if type(msg) is list:
+            for line in msg:
+                self.__content.append(str(line))
+        else:
+            self.__content.append(str(msg))
     
-    def append(self,arr):
-        '''Add new lines from a given array.'''
-        for line in arr:
-            self.__content.append(str(line))
-
 
     def pop(self,index=-1):
         '''Deletes a line from the box.'''
@@ -135,15 +138,16 @@ class createBox:
     def printBox(self):
         '''Prints the box.'''
         self.__updateDimensions()
-        line_width=self.__width -2*self.__margin -2*len(self.__vertical_symbol)
+        line_width=self.__width -2*self.__margin -2*len(self.__vSymbol)
         fmt = '{hPadding}{edge}{margin}{text:{allignSign}{width}}{margin}{edge}' 
-        top_buttom_fmt = ' '*self.__hPadding +self.__horizontal_symbol*self.__width
+        top_buttom_fmt = ' '*self.__hPadding +self.__hSymbol*int(self.__width
+                                                           /len(self.__hSymbol))
 
         print(top_buttom_fmt) #The top of the box.
 
         for i in range(self.__height):
             line = '' if i >= len(self.__content) else self.__content[i]
-            print(fmt.format(edge = self.__vertical_symbol,
+            print(fmt.format(edge = self.__vSymbol,
                              margin =' '*self.__margin,
                              text = line,
                              hPadding =' '*self.__hPadding,
